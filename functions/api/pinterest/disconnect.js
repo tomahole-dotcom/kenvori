58cb544a8f02b2bedee278c14ab59be6323eee2d
tomahole-dotcom <tomahole@gmail.com>
2026-08-27 13:03:17 +0200

    Add Pinterest disconnect endpoint

---
+++ functions/api/pinterest/disconnect.js
@@ -0,0 +1,9 @@
+export async function onRequestPost() {
+  const headers = new Headers({
+    'Content-Type': 'application/json; charset=utf-8',
+    'Cache-Control': 'no-store'
+  });
+  headers.append('Set-Cookie', 'kenvori_pinterest_token=; Path=/api/pinterest; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
+  headers.append('Set-Cookie', 'kenvori_pinterest_state=; Path=/api/pinterest; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
+  return new Response(JSON.stringify({ ok: true }), { status: 200, headers });
+}
