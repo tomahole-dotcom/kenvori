580cf362bb2e3152008d7d09e757dc1ad7d2f182
tomahole-dotcom <tomahole@gmail.com>
2026-08-27 13:16:21 +0200

    Clear Pinterest auth cookies reliably

--- functions/api/pinterest/disconnect.js
+++ functions/api/pinterest/disconnect.js
@@ -3,6 +3,7 @@ export async function onRequestPost() {
     'Content-Type': 'application/json; charset=utf-8',
     'Cache-Control': 'no-store'
   });
+  headers.append('Set-Cookie', 'kenvori_pinterest_token=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
   headers.append('Set-Cookie', 'kenvori_pinterest_token=; Path=/api/pinterest; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
   headers.append('Set-Cookie', 'kenvori_pinterest_state=; Path=/api/pinterest; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
   return new Response(JSON.stringify({ ok: true }), { status: 200, headers });
