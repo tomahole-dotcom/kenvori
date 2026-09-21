2bbbc098bd85764a4160fcba1352a40c9ceafa3e
tomahole-dotcom <tomahole@gmail.com>
2026-09-21 19:12:31 +0200

    Automate Etsy section and phone-case variants

--- pod-factory/src/candidate-0002.js
+++ pod-factory/src/candidate-0002.js
@@ -13,6 +13,6 @@ export const candidate0002=createPodCandidate({
  candidateKey:"candidate-0002",theme:"whimsical polka dots",productType:"phone case",
  artworkUrl:"https://a62856d4-05af-4f1b-82d6-b6b6d2d1e5d8.sandbox.floot.app/_cdn/static/6151d3bb-a99f-4203-b3c5-29416eee51b5-kenvori-whimsical-polka-dots-phone-case-master.png",
  printifyProductId:"6ab0dfb533dcede08c071937",etsyListingId:4579711218,priceCents:2972,
- researchApproved:true,creativeApproved:true,productFitApproved:true,actualCostKnown:true,artworkReady:true,technicalQaPass:true,mockupQaPass:true,variantCoverageVerified:true,ipGreen:true,etsyPackageReady:true,
+ researchApproved:true,creativeApproved:true,productFitApproved:true,actualCostKnown:true,artworkReady:true,technicalQaPass:true,mockupQaPass:true,variantCoverageVerified:true,ipGreen:true,etsyPackageReady:false,
  safeMode:true,publishAuthorization:false
 });
